687cce2b46abb913008cd9aa8705ed759bb9a89f
vpamrit <vishnuamritpydah@gmail.com>
2024-06-26 11:56:44 -0700

    updating configs to try to work with custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
   ChakraProvider,
 } from "@chakra-ui/react";
 
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import WebsiteInProgress from "./pages/Home/WebsiteInProgress.tsx";
 
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <ChakraProvider>
-      <HashRouter>
+      <BrowserRouter basename={"/"}>
         <div className="App">
           <header className="App-header">
             <Nav />
@@ -32,7 +32,7 @@ function App() {
             </Routes>
           </header>
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
